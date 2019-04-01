5add5fe74ce36eb544663b6fae2902f2df10c888
Sara Krum <sarasmith887@gmail.com>
2019-04-01 19:08:37 -0400

    header components styled

--- habit-tracker/src/App.js
+++ habit-tracker/src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import userIcon from './images/user-icon.svg';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          Habit Tracker
+      <div>
+        <header className="header">
+          <h1>Habit Tracker</h1>
         </header>
-      </div>
+
+        <header className="headerLogin">
+          <h1>Habit Tracker</h1>
+          <div className="user">
+            <p>Hello, <img src={userIcon} alt="user icon"/></p>
+            <p>User Name</p>
+          </div>
+        </header>
+        </div>
     );
   }
 }
