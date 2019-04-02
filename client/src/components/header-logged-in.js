456bd14040340ebbafbc7e355d8af73ede413ed4
Paul Gamble <pgamble@gmail.com>
2019-04-02 11:47:42 -0400

    merge PR #36

---
+++ client/src/components/header-logged-in.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import UserIcon from '../images/user-icon.svg'
+
+const HeaderLoggedin = (props) => {
+    return (
+        <header className="headerLogin">
+            <h1>Habit Tracker</h1>
+            <div className="user">
+            <p>Hello, <img src={UserIcon} alt="user icon"/></p>
+            <p>User Name</p>
+            </div>
+      </header>
+    );
+}
+
+export default HeaderLoggedin;
