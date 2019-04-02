fc2ad00bd14c57fe77b2d7042a3cef538ea36727
Paul Gamble <pgamble@gmail.com>
2019-04-02 11:38:51 -0400

    merged PR #36 locally

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
 import Login from '../components/Login';
 import Dashboard from '../components/Dashboard';
 import About from '../components/About';
 import withAuthentication from '../containers/withAuthentication';
-
+import Header from '../components/header'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+     
       <Router>
+        
         <Switch>
           <Route path="/" exact component={Login} />
           <Route path="/dashboard" component={withAuthentication(Dashboard)} />
