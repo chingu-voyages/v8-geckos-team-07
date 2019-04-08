c3f7a81f1643483aa3da5a76d42b7717b03fd8f5
Sara Krum <sarasmith887@gmail.com>
2019-04-08 17:11:23 -0400

    form component created

---
+++ client/src/components/check-in.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const CheckIn = (props) => {
+    return (
+
+    <form className="daily-check-in">
+        <h2>Daily Check In</h2>
+            
+    </form>
+    );
+}
+export default CheckIn;
