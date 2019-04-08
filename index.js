3cc62f4186767b1c649dd56714b00434c450c3b3
Paul Gamble <pgamble@gmail.com>
2019-04-08 18:37:24 -0400

    removed rubbish from path

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use('/api/habits', habits);
 
 //TESTING - https://tylermcginnis.com/react-router-cannot-get-url-refresh/
 app.get('/*', function (req, res) {
-	res.sendFile(path.join(__dirname, 'path/to/your/index.html'), function (err) {
+	res.sendFile(path.join(__dirname, 'index.html'), function (err) {
 		if (err) {
 			res.status(500).send(err)
 		}
