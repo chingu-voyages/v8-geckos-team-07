7a9706a469c6d13926cc4a454787a92352253ea2
Paul Gamble <pgamble@gmail.com>
2019-04-08 20:43:46 -0400

    Fix/backend routing (#47)
    * Create issue_template.md
    
    * Update issue templates
    
    * Update issue templates
    
    * Update issue templates
    
    * added static.json
    
    * Packge.json: Run npm install in client folder everytime you run npm install on main app
    
    * Load DB Key from .env
    
    -Added dotenv npm package
    -Remove keys.js
    -Load DB key from .env file
    
    * minor change to redo build on heroku
    
    * added express routing code
    
    * removed rubbish from path
    
    * specified client/build/index.html
    
    * routing resolved
    
    * removed testing string

--- index.js
+++ index.js
@@ -23,6 +23,17 @@ mongoose.connect(db)
 // Use Routes
 app.use('/api/habits', habits);
 
+
+// Point to client's index
+app.get('*', function (req, res) {
+	res.sendFile(path.join(__dirname, '/client/build/index.html'), function (err) {
+		if (err) {
+			res.status(500).send(err)
+		}
+	})
+})
+
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log('Server started on port ' + port));
