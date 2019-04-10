678505822e1461c04b1327c3d2eedf78615a1561
Paul Gamble <pgamble@gmail.com>
2019-04-10 11:55:37 -0400

    remove unused link import from Login.js

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-
 import Layout from '../containers/Layout';
 import SocialButtonList from './SocialButtonList';
 import { auth } from '../firebase';
