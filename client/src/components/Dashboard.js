5cf4b922810179905342b35bafae419a09c0ae55
Paul Gamble <pgamble@gmail.com>
2019-04-10 11:00:18 -0400

    removed h1 from dashboard

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -77,7 +77,7 @@ class Dashboard extends Component {
     render() {
         return (
             <Layout>
-                <h1>User Logged In</h1>
+               
                 <SocialProfileList
                     auth={auth.getAuth}
                     providerData={this.state.providerData}
