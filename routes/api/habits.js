be065b631b708f060bbf0f5d9c096d0ebc9327d9
Patrick <patricktownley@users.noreply.github.com>
2019-04-12 15:24:27 -0700

    Backend Work for Checkin Submission (#67)
    * Update Schema to include Array of checkins
    
    * Add API method to complete a checkin for a given habit
    
    * Add functionality to checkin form
    
    - Basically borrowed all the concepts from the New Habit form, albeit with less validation
    - Temporarily added Habit field for manual habit ID entry
    - Confirmed that it can update habits on the backend

--- routes/api/habits.js
+++ routes/api/habits.js
@@ -19,6 +19,24 @@ router.post('/newhabit', (req, res) => {
 	newHabit.save().then(habit => res.json(habit));
 });
 
+//@route 	POST api/checkin
+//@desc 	Perform a checkin for one of your habits
+//@access	Public
+router.post('/checkin', (req, res) => {
+	const checkin = {
+		// Date will be autofilled
+		effort: req.body.mood,
+		mood: req.body.mood,
+		selfEval: req.body.selfEval,
+		notes: req.body.notes
+	}
+	// Just to get this off the ground - request should include habit id
+	// Probably better use combination of user id + habit name
+	Habit.findOneAndUpdate({_id: req.body.habit}, {$push: {checkins: checkin}})
+		.then(habit => res.json(habit));
+});
+
+
 //@route 	GET api/items
 //@desc 	Get all habits
 //@access	Public
