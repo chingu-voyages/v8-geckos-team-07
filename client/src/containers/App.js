4e0af6c3b12a196ee47190cf349f9872b5ec0f11
Sara Krum <sarasmith887@gmail.com>
2019-04-13 16:54:56 -0400

    default page adjusted and styled

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -18,10 +18,8 @@ class App extends Component {
           <Route path="/newhabit" component={NewHabit} />
           <Route path="/checkin" component={CheckIn} />
           <main>
-            <h2>Test Heading Level 2</h2>
-            <h3>This is a Heading Level 3</h3>
-            <p>Paragraphs of text look like this.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus rhoncus id elit ut ultrices. Nunc pretium lacus quis aliquam tempus. Nullam non fermentum tellus, eu semper massa. Nunc at libero augue. Curabitur est nunc, auctor viverra interdum a, fringilla vel ex. Aliquam tellus urna, accumsan sed ex ac, rhoncus aliquet turpis. Morbi at sapien vel ipsum malesuada luctus. Proin efficitur iaculis ipsum, eget blandit nulla commodo ut. Proin at elit sit amet enim mollis semper eget quis nisl. Suspendisse sed metus ante. Ut venenatis nibh sit amet leo sollicitudin rutrum. In hac habitasse platea dictumst. Nullam ac nisi et lorem accumsan vestibulum eu sed erat.</p>
+            <h2>404 Error</h2>
+            <p>App.js is the default page.</p>
           </main >
         </Switch>
       </Router>
