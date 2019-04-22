dd0e5fc8bfaabd7bf185cad6b73e4d31af712862
Paul Gamble <pgamble@gmail.com>
2019-04-22 00:30:11 -0400

    🐛  - fixed up the display copying bootstrap ref

--- client/src/components/Progress.js
+++ client/src/components/Progress.js
@@ -6,18 +6,28 @@ class Progress extends Component {
     
     render() {
         return ( 
-            <CalendarHeatmap
-                startDate={new Date('2016-01-01')}
-                endDate={new Date('2016-04-01')}
-                values={[
-                    { date: '2016-01-01' },
-                    { date: '2016-01-22' },
-                    { date: '2016-01-30' },
-                    // ...and so on
-                ]}
-            />
-
-
+            <div>
+                <div className="row">
+                    <div className="col-12 col-sm-6">
+                
+                
+                        <CalendarHeatmap
+                            startDate={new Date('2019-01-01')}
+                            endDate={new Date('2019-05-01')}
+                            values={[
+                                { date: '2019-04-20' },
+                                { date: '2019-04-21' },
+                                { date: '2016-04-22' },
+                                { date: '2016-04-22' },
+                                { date: '2016-04-22' },
+                                { date: '2016-04-23' },
+                                // ...and so on
+                            ]}
+                        />
+                      
+                    </div>
+                </div>
+            </div>
         )
     
     }
