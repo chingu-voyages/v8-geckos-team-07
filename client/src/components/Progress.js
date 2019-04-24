c5e68c528e90bce7b876ae59ee759f923a4ffdc4
Paul Gamble <pgamble@gmail.com>
2019-04-23 21:53:23 -0400

    🐛  - tweaked footer css; remove bootstrap

--- client/src/components/Progress.js
+++ client/src/components/Progress.js
@@ -8,26 +8,23 @@ class Progress extends Component {
     render() {
         return ( 
             <div>
-                <div className="row">
-                    <div className="col-12 col-sm-6">
+                
                 
                 
                         <CalendarHeatmap
                             startDate={new Date('2019-01-01')}
-                            endDate={new Date('2019-05-01')}
+                            endDate={new Date('2019-12-01')}
                             values={[
                                 { date: '2019-04-20' },
                                 { date: '2019-04-21' },
-                                { date: '2016-04-22' },
-                                { date: '2016-04-22' },
                                 { date: '2016-04-22' },
                                 { date: '2016-04-23' },
                                 // ...and so on
                             ]}
                         />
                       
-                    </div>
-                </div>
+                  
+               
                 <ReactTooltip />
             </div>
         )
