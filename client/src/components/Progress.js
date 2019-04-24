65c9929a8333dfdb309e234907339d16c0469f0d
Paul Gamble <pgamble@gmail.com>
2019-04-23 23:51:03 -0400

    💄  - made the calendar heatmap a sep component

--- client/src/components/Progress.js
+++ client/src/components/Progress.js
@@ -1,34 +1,15 @@
 import React, { Component } from 'react';
-import CalendarHeatmap from 'react-calendar-heatmap';
-import 'react-calendar-heatmap/dist/styles.css';
-import ReactTooltip from 'react-tooltip';
+import CalHM from '../components/CalHM';
+
 
 class Progress extends Component {
     
     render() {
         return ( 
             <div>
-                
-                
-                
-                        <CalendarHeatmap
-                            startDate={new Date('2019-01-01')}
-                            endDate={new Date('2019-12-01')}
-                            values={[
-                                { date: '2019-04-20' },
-                                { date: '2019-04-21' },
-                                { date: '2016-04-22' },
-                                { date: '2016-04-23' },
-                                // ...and so on
-                            ]}
-                        />
-                      
-                  
-               
-                <ReactTooltip />
+                <CalHM />    
             </div>
         )
-    
     }
 }
 export default Progress;
