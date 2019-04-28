80f948096358d003d26860af6c8894377d4cafed
Sara Krum <sarasmith887@gmail.com>
2019-04-28 09:45:32 -0400

    text added to login and footer, footer centered

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -4,22 +4,13 @@ const Footer = (props) => {
     return (
 
         <footer id="footer">
+            <p>Chingu Voyage 8 <span role="img" aria-label="heart emoji"> ❤️</span> <a href="https://github.com/chingu-voyages/v8-geckos-team-07" target="_blank" rel="noopener noreferrer">Gecko Team 7 Project</a></p>
 
-            <div id="special_thanks">
-                <p>
-                    Thank you {' '}
-                    <span role="img" aria-label="heart emoji">
-                        ❤️
-            </span>{' '}
-                    <a href="https://blog.bitsrc.io/react-oauth-authentication-with-firebase-dfe0e8c5d0d4" target="_blank" rel="noopener noreferrer">Esau Silva</a>
+                <p>Thank you <span role="img" aria-label="heart emoji"> ❤️</span>
+                <a href="https://blog.bitsrc.io/react-oauth-authentication-with-firebase-dfe0e8c5d0d4" target="_blank" rel="noopener noreferrer">Esau Silva</a>
                 </p>
-            </div>
-
-
         </footer>
     );
 }
 
 export default Footer;
-
-
