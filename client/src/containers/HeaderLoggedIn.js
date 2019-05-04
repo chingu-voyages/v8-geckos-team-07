339c4895b61a73c8e1884cc4b846c70ae1e0d0ef
Sara Krum <sarasmith887@gmail.com>
2019-05-04 14:59:40 -0400

    about text added, slideout width increased

--- client/src/containers/HeaderLoggedIn.js
+++ client/src/containers/HeaderLoggedIn.js
@@ -23,6 +23,7 @@ const HeaderLoggedIn = ({ children, hamburgerToggle, hamburgerOpen }) => {
                     <nav id="sidebar" className={hamburgerOpen ? "open" : null }>
                             <Link to="/UserSettings" ><h3>User Settings</h3></Link>
                             <h3>About</h3>
+                                <p>To use Habit Tracker, users need to check-in daily and reflect on their progress. Use the “Check In” button to log your daily progress. Note if you already checked-in for the day that button will not be visible. A check-in will color a square on the calendar based on the user’s self-evaluation of: killed it, completed, tried, failed, or skip. Users can view their information by hovering on the calendar squares. Clicking on a square will show the data for that day.</p>
                             <h3>Resources</h3>
                                 <ul>
                                     <li><a href="https://blog.bitsrc.io/react-oauth-authentication-with-firebase-dfe0e8c5d0d4" target="_blank" rel="noopener noreferrer">Esau Silva</a></li>
