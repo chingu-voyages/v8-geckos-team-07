69b56d85379570af5107d6814b88afcd0210ad4c
Paul Gamble <pgamble@gmail.com>
2019-10-30 18:30:32 -0400

    👽 tweak api

--- client/src/firebase/firebase.js
+++ client/src/firebase/firebase.js
@@ -3,7 +3,7 @@ import 'firebase/auth';
 //import 'firebase/database';
 
 const app = firebase.initializeApp({
-  apiKey: REACT_APP_FIREBASE_API_KEY = "AIzaSyDiMOkjJt4xSUeO4PbrWOjihwxXMtYh-gA",
+  apiKey: 'AIzaSyDiMOkjJt4xSUeO4PbrWOjihwxXMtYh-gA',
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN
   //databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL
 });
